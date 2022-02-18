87f8ce68cdfe38223077f81e7468e058ae56c509
MounikaAlloju <34786164+ALLOJU@users.noreply.github.com>
2022-02-18 08:34:12 +0530

    added count

--- src/com/bridgelabz/SumOfTriplets.java
+++ src/com/bridgelabz/SumOfTriplets.java
@@ -7,10 +7,11 @@ public class SumOfTriplets {
 	
 	private static void findTriplets(int[] arr, int n) {
 		boolean found=true;
-		
+		int count=0;
 		for(int i=0;i<n-2;i++) {
 			for(int j=i+1;j<n-1;j++) {
 				for(int k=j+1;k<n;k++) {
+					count=count+1;
 					if(arr[i]+arr[j]+arr[k] == 0) 
 					{
 						System.out.print(arr[i]); 
@@ -24,6 +25,8 @@ public class SumOfTriplets {
 				}
 			}
 		}
+		
+		System.out.println("Triplet Count  "+count);
 		if(found==false) {
 			System.out.println("Triplet not exist");
 		}
